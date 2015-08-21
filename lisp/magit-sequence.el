442d6d13cafa23b5a311b00fb05063c40f1ce1ce
Jun Inoue <inoue@amgdh183.o.aist.go.jp>
2015-08-21 18:17:38 +0900

    Warn if rebase abort can cause permanent data loss
    Git's rebase abort command will happily erase every change, both
    committed and uncommitted.  Of these, the uncommitted changes will
    become inaccessible even to reflog digging.  I sometimes leave a rebase
    unfinished, then come back and make changes, realize something is amiss,
    do a rebase abort, and boom! permanent loss of work.  This brief warning
    prevents this tragedy.

--- lisp/magit-sequence.el
+++ lisp/magit-sequence.el
@@ -482,7 +482,12 @@ edit.  With a prefix argument the old message is reused as-is."
   "Abort the current rebase operation, restoring the original branch."
   (interactive)
   (if (magit-rebase-in-progress-p)
-      (magit-run-git "rebase" "--abort")
+      (when (or (not (magit-anything-modified-p))
+                (yes-or-no-p (concat "You have uncommited changes"
+                                     " that will be lost even to "
+                                     "reflog digging.  Are you "
+                                     "sure you want to abort?")))
+        (magit-run-git "rebase" "--abort"))
     (user-error "No rebase in progress")))
 
 (defun magit-rebase-in-progress-p ()
